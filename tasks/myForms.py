88c07e0d75ef0372bfb765c0cac86025ef811c2d
Vitor Lima <viitorlima2000@gmail.com>
2025-04-20 07:00:56 -0300

    Update myForms.py

--- tasks/myForms.py
+++ tasks/myForms.py
@@ -13,3 +13,17 @@ class TaskSearchCompleted(forms.Form):
         queryset=Tasks.objects.filter(completed=True),
         label="Select a completed task to search for"
     )
+
+class CreateTask(forms.ModelForm):
+    class Meta:
+        model = Tasks
+        fields = ['title', 'category', 'description', 'due_date']
+        widgets = {
+            'due_date': forms.DateInput(attrs={'type': 'date'}),
+        }
+        labels = {
+            'title': 'Task Title',
+            'category': 'Category',
+            'description': 'Description',
+            'due_date': 'Due Date',
+        }
